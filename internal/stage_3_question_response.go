acea43a75ffc459817e80a44c61e4f780ec80e27
Sherub Thakur <sherub.thakur@gmail.com>
2023-11-06 13:07:58 +0530

    Better error message in stage 3

--- internal/stage_3_question_response.go
+++ internal/stage_3_question_response.go
@@ -22,11 +22,9 @@ func testReceiveQuestionInResponse(stageHarness *tester_utils.StageHarness) erro
 		return fmt.Errorf("Error sending DNS query: %s\n", err)
 	}
 
-	if response.Id != DEFAULT_PKT_ID {
-		return fmt.Errorf("Expected ID to be 1234, got %d", response.Id)
-	}
-	if len(response.Question) == 0 {
-		return fmt.Errorf("Expected question section to have one entry got %d", len(response.Question))
+	if len(response.Question) != 1 {
+		helpMsg := "If you have question set in question section and are still seeing this error, make sure that you are setting the QCOUNT to 1 in the header section too."
+		return fmt.Errorf("Expected question section to have one entry got %d\n%s", len(response.Question), helpMsg)
 	}
 	if response.Question[0].Name != DEFAULT_DOMAIN {
 		return fmt.Errorf("Expected question domain name to be `%v` got `%v`", DEFAULT_DOMAIN, response.Question[0].Name)
