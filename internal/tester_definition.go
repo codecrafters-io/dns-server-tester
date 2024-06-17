59c43f1457d63d54261434559ab501f575fc2faa
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-06-17 14:51:36 +0530

    Update tester definition and tester-utils

--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -5,8 +5,9 @@ import (
 )
 
 var testerDefinition = tester_definition.TesterDefinition{
-	AntiCheatTestCases: []tester_definition.TestCase{},
-	ExecutableFileName: "your_server.sh",
+	AntiCheatTestCases:       []tester_definition.TestCase{},
+	ExecutableFileName:       "your_program.sh",
+	LegacyExecutableFileName: "your_server.sh",
 	TestCases: []tester_definition.TestCase{
 		{
 			Slug:     "ux2",
